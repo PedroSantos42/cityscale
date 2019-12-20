e3a2c19a179942b5a73fae5aa3c1645ef924e800
Yoshiiro07 <59020911+Yoshiiro07@users.noreply.github.com>
2019-12-20 11:10:39 -0300

    Adicionada descrição de cores para caso de edição
    Adicionada descrição de cores para caso de edição

--- CityScale/gdx-game/src/com/moonbolt/cityscale/Damage.java
+++ CityScale/gdx-game/src/com/moonbolt/cityscale/Damage.java
@@ -8,3 +8,9 @@ public class Damage {
 	public String Color;
 	public String Descritivo;
 }
+
+
+//Descrição de cores
+// 1 - Vermelho - Dano do Inimigo
+// 2 - Amarelo - Dano Causado pelo jogador
+// 3 - Verde - Recuperação
